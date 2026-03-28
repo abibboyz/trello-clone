ab64465c23fe8a44dff40a192488ff56783f6aee
unknown <abi.kh03@gmail.com>
2026-03-29 10:29:50 +1100

    updated schema init

---
+++ backend/app/schemas/__init__.py
@@ -0,0 +1,9 @@
+from .board import Board, BoardCreate
+from .list import List, ListCreate
+from .card import Card, CardCreate
+
+__all__ = [
+    "Board", "BoardCreate",
+    "List", "ListCreate",
+    "Card", "CardCreate",
+]
